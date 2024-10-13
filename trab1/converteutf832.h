333747e56e4cb1162726282b179b43df1c5257a0
gabripatricio <155901470+gabripatricio@users.noreply.github.com>
2024-10-13 17:05:53 -0300

    Update converteutf832.h

--- trab1/converteutf832.h
+++ trab1/converteutf832.h
@@ -1,10 +1,3 @@
-#define CONVERTEUTF832_H
-
-#include <stdio.h>
-
-// Função que converte de UTF-8 para UTF-32 (little-endian)
 int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida);
-
-// Função que converte de UTF-32 (little-endian ou big-endian) para UTF-8
 int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida);
 
