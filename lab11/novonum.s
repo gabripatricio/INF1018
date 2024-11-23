a8d95ba5f4eadf1da6be6650d481d45f5ab5eab0
gabripatricio <155901470+gabripatricio@users.noreply.github.com>
2024-11-23 17:44:42 -0300

    Update novonum.s

--- lab11/novonum.s
+++ lab11/novonum.s
@@ -2,32 +2,27 @@
 Sf: .string "numero: "
 percent_d: .string "%d"
 
+.text
 
 .globl novonum
 
-.text
 novonum:
-
-pushq %rbp # RA
-movq %rsp, %rbp # RA
-subq $16, %rsp # abrindo espaco na pilha
-
-
-movq $Sf, %rdi
-movl $0, %eax
-call printf # nao tem segundo parametro
-
-
-movq $percent_d, %rdi
-# leaq -4(%rbp), %rsi # load effective address de -4 rbp
-
-# podemos fazer o leaq assim:
-
-movq %rbp, %rsi
-subq $4, %rsi
-
-call scanf
-movl -4(%rbp), %eax
-
-leave
-ret
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $16, %rsp
+    
+    movq $Sf, %rdi
+    call printf
+    
+    movq $percent_d, %rdi
+    
+    # leaq -4(%rbp), %rsi # load effective address de -4 rbp
+    
+    movq %rbp, %rsi # iguala o endereco a uma regiao da pilha (leaq)
+    subq $4, %rsi
+    call scanf
+    
+    movl -4(%rbp), %eax
+    
+    leave
+    ret
