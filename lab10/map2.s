62680787cd32924310654e37a0499ebece6e4fab
gabripatricio <155901470+gabripatricio@users.noreply.github.com>
2024-11-23 15:29:24 -0300

    Update map2.s

--- lab10/map2.s
+++ lab10/map2.s
@@ -1,40 +1,45 @@
-# OBSERVACAO: NAO DEU TEMPO DE TERMINAR... TALVEZ EDITE NO FUTURO.
+.text
 
-.text 
 .globl map2
 
 map2:
-
-    # inicio ... 
     pushq %rbp
     movq %rsp, %rbp
+    subq $32, %rsp
+
+    movl $0, %ecx
+    
+    loop:
+        cmpl %edx, %ecx # i < n ????
+        jge fim
+
+        movl %ecx, %r10d
+        imull $4, %r10d
+        
+        movq %rdi, -8(%rbp)
+        movq %rsi, -16(%rbp)
+        movl %ecx, -20(%rbp)
+        movl %r10d, -24(%rbp) 
+
+        addq %r10, %rdi
+        addq %r10, %rsi
 
 
-    movl $0, %ecx # i = 0
+        movl (%rsi), %edi
+        call f
 
-loop:
+        movq -8(%rbp), %rdi
+        movq -16(%rbp), %rsi
+        movl -20(%rbp), %eax
+        movl -24(%rbp), %r10d
 
-    cmpl %ecx, %edx
+        movq %rsi, %r9
+        addq %r10, %r9
+        movl %eax, (%r9)        
 
-    je fim
+        addl $1, %ecx # i++
+        jmp loop
 
-    movl (%rsi, %rcx, 4), % ebx # *(outro + i)
-
-    pushq %rdi # salva o registrador
-
-
-    movl (%rdi, %rcx, 4), %edi # chamado com *( um+i)
-    call f
-
-    movl %eax, %ebx
-    popq %rdi
-
-    addl $1, %ecx  
-
-    jmp loop
-
-
-fim:
-    # retornando
-    leave 
-    ret
+    fim:
+        leave
+        ret
