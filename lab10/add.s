a141b09f2a118c3d3bb37954ca3adda6780f6fed
gabripatricio <155901470+gabripatricio@users.noreply.github.com>
2024-11-23 17:28:47 -0300

    Update add.s

--- lab10/add.s
+++ lab10/add.s
@@ -3,30 +3,22 @@
 .globl add
 
 add:
-
-
-# inicio (abrindo espaco na pilha)
-pushq %rbp
-movq %rsp, %rbp
-
-cmpq $0, %rdi # x == NULL?
-
-je end
-
-movl (%rdi), %ebx # x -> val
-
-movq 8(%rdi), %rdi # tem que aumentar pois vamos chamar a funcao com x->next
-call add  # que vai me retornar em %eax
-
-addl %ebx, %eax
-
-
-# return
-
-leave
-ret
-
-end:
-movl $0, %eax
-leave
-ret
+    pushq %rbp
+    movq %rsp, %rbp
+    
+    movl $0, %ecx
+    
+    loop:
+    cmpq $0, %rdi
+    je fim
+    
+    addl (%rdi), %ecx
+    
+    movq 8(%rdi), %rdi 
+    jmp loop
+    
+    fim:
+    movl %ecx, %eax
+    leave
+    ret
+    
