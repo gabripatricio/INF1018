7fdd66ec1f79c724291d1f5da0da0ff86c5df68c
gabripatricio <155901470+gabripatricio@users.noreply.github.com>
2024-11-23 18:00:45 -0300

    Update bemboba.s

--- lab11/bemboba.s
+++ lab11/bemboba.s
@@ -1,40 +1,34 @@
+.text
+
 .globl bemboba
 
 bemboba:
-
-pushq %rbp
-movq %rsp, %rbp
-
-# abrindo espaco na pilha
-subq $32, %rsp # ja abri logo 32 pois seriam 16 do vetor e mais alguma outra coisa qualquer
-movq %rbx, -8(%rbp) # salvando o rbx
-
-movl $0, %ecx
-
-# leaq -32(%rbp), %rbx # depois tem que lembrar de salvar esse registrador
-movq %rbp, %rbx
-subq $32, %rbx # -32 rbp eh p endereco de rbx
-
-
-# a = local
-movq %rbx, %r9
-loop:
-
-cmpl $4, %ecx
-jge fim_for
-
-movl %edi, (%r9) # *a = num
-addq $4, %r9 # a++
-addl $1, %ecx
-
-jmp loop
-
-fim_for:
-
-movq %rbx, %rdi # passando o endereco
-movl $4, %esi
-
-call addl # ja devolve de em %eax
-movq -8(%rbp), %rbx # recupera o valor de rbx
-leave
-ret
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $32, %rsp # 16 do vetor, mais alguma tralha 
+    
+    movl $0, %ecx
+    movq %rbp, %rdx
+    subq $16, %rdx # rdx = -16(%rbp)
+    
+    movq %rdx, -24(%rbp) # apenas salvando o endereco para usar depois
+    
+    loop:
+    cmpl $4, %ecx
+    jge fim
+    
+    movl %edi, (%rdx)
+    
+    addq $4, %rdx
+    addl $1, %ecx
+    jmp loop
+    
+    
+    fim:
+    movq -24(%rbp), %rdi # pegando o endereco de volta para passar corretamente
+    movl $4, %esi 
+    call addl
+    
+    leave
+    ret
+    
