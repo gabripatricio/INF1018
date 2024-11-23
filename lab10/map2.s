b48f9574f0f05ef6e3d5fd8c03c70b8b74675e0a
gabripatricio <155901470+gabripatricio@users.noreply.github.com>
2024-11-23 17:22:39 -0300

    Update map2.s

--- lab10/map2.s
+++ lab10/map2.s
@@ -1,1 +1,42 @@
+.text
+.globl map2
 
+map2:
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $32, %rsp
+    
+    movl $0, %ecx
+    movq %rdi, -8(%rbp)
+    movq %rsi, -16(%rbp)
+    
+    loop:
+    cmpl %edx, %ecx
+    jge fim
+    
+    movl %ecx, -20(%rbp)
+    
+    imull $4, %ecx
+    addq %rcx, %rdi
+    movl (%rdi), %edi
+    
+    movl %ecx, -24(%rbp)
+    call f 
+    
+    movq -8(%rbp), %rdi
+    movq -16(%rbp), %rsi
+    movl -20(%rbp), %ecx
+    movl -24(%rbp), %r9d
+    
+    movq %rsi, %r10
+    addq %r9, %r10
+    
+    movl %eax, (%r10)
+    
+    incl %ecx
+    jmp loop
+    
+    fim:
+    leave
+    ret
+    
